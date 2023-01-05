67981e7618622dee86c189aa5a69dc7680af4d96
teruo.ouchi <marcos.teruo.ouchi@gmail.com>
2023-01-04 21:00:18 -0300

    Tipos e Variáveis. Operadores lógicos em LUA

---
+++ TiposeVariaveis.lua
@@ -0,0 +1,23 @@
+-- Declarando algumas variáveis
+
+local energia = 3
+local nome = "Teruo"
+local passoufase = false
+
+-- Somando valores
+local maisenergia = 4
+print(energia + maisenergia)
+
+-- Concatenar texto
+print("Nome do Jogador: " .. nome)
+
+-- Verificar tipos
+print(type(energia))
+print(type(nome))
+print(type(passoufase))
+
+-- Operadores Relacionais
+print(energia == maisenergia)
+print(energia ~= maisenergia)
+print(energia > maisenergia)
+
