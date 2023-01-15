0017af9c081a24f6c44bf993e01d33d678e86bf9
teruo.ouchi <marcos.teruo.ouchi@gmail.com>
2023-01-14 21:17:45 -0300

    Estruturas de repetição - while

---
+++ decisao2.lua
@@ -0,0 +1,11 @@
+-- Condição Lógica
+-- While, For e Repeat
+
+local energia = 10
+
+while (energia >= 0) do
+    print("Vivo, com energia igual a " .. energia)
+    energia = energia-1
+end
+
+print("MORTO! Acabou a energia")
